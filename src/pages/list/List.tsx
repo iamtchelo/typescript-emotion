28ef8cb71dea439911b78f6a28344631d2d92270
Marcelo Silva <marcelosilva.developer@gmail.com>
2018-06-19 21:42:08 -0300

    Show pokemons with real data

--- src/pages/list/List.tsx
+++ src/pages/list/List.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import pokemons from '../../data/mock';
 import { 
   Card,
   ListContent,
@@ -8,17 +9,14 @@ import {
   Title
 } from './list.elements';
 
-const arr = Array.from({ length: 60 }, (n, i) => i + 1);
-const pokemonImage = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back';
-
 const List: React.SFC = props => (
   <ListWrapper>
     <Title size={1.5}>TypeScript + Emotion = ♥</Title>
     <ListContent>
-      {arr.map(v =>
-        <Card key={v}>
-          <PokemonAvatar src={`${pokemonImage}/${v}.png`} />
-          <PokemonName>Picachu</PokemonName>
+      {pokemons.map(pokemon =>
+        <Card key={pokemon.name}>
+          <PokemonAvatar src={pokemon.avatar} />
+          <PokemonName>{pokemon.name}</PokemonName>
         </Card>
       )}
     </ListContent>
