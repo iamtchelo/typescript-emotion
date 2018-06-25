01b0cf3fbb5f17110c3901877268987b8c46e31b
Marcelo Silva <marcelosilva.developer@gmail.com>
2018-06-25 01:14:51 -0300

    Update tests and snapshots

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,16 +1,17 @@
 import { render, shallow } from 'enzyme';
+import { renderToJson } from 'enzyme-to-json';
 import * as React from 'react';
 import Button from './Button';
 
 describe('<Button />', () => {
   test('should render with default style', () => {
     const tree = render(<Button>My button</Button>);
-    expect(tree).toMatchSnapshot();
+    expect(renderToJson(tree)).toMatchSnapshot();
   });
 
   test('should render with actived style', () => {
-    const tree = render(<Button>My button</Button>);
-    expect(tree).toMatchSnapshot();
+    const tree = render(<Button actived={true}>My button</Button>);
+    expect(renderToJson(tree)).toMatchSnapshot();
   });
 
   test('should simulate the click button', () => {
--- src/components/Navbar/Navbar.test.tsx
+++ src/components/Navbar/Navbar.test.tsx
@@ -1,10 +1,11 @@
 import { render } from 'enzyme';
+import { renderToJson } from 'enzyme-to-json';
 import * as React from 'react';
 import Navbar from './Navbar';
 
 describe('<Navbar />', () => {
   test('should render a default navbar with a title', () => {
     const tree = render(<Navbar title="My navbar" />);
-    expect(tree).toMatchSnapshot();
+    expect(renderToJson(tree)).toMatchSnapshot();
   });
 });
