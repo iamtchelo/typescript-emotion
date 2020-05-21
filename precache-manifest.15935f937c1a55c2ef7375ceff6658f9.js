e7edbb64f4689ceb4e437ceda5b89e009f1439f3
Marcelo Silva <marcelosila.developer@gmail.com>
2020-05-21 20:29:51 -0300

    Updates

---
+++ precache-manifest.15935f937c1a55c2ef7375ceff6658f9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "56b1aacf1a08092dfec2",
+    "url": "/typescript-emotion/static/js/main.56b1aacf.chunk.js"
+  },
+  {
+    "revision": "f604d61ab287c76c8c10",
+    "url": "/typescript-emotion/static/js/1.f604d61a.chunk.js"
+  },
+  {
+    "revision": "25774fca0161117a00e9",
+    "url": "/typescript-emotion/static/js/runtime~main.25774fca.js"
+  },
+  {
+    "revision": "7ced8145bf4edebe74580e014f5d95b6",
+    "url": "/typescript-emotion/index.html"
+  }
+];
