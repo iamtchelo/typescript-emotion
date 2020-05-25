1310ff8bb7dfbc77c813fb2de976b7a478e57bfc
Marcelo Silva <marcelosilva.developer@gmail.com>
2020-05-25 00:08:51 -0300

    Add base layout on list page

--- src/pages/list/List.tsx
+++ src/pages/list/List.tsx
@@ -1,66 +1,13 @@
 import * as React from 'react';
-import {Button, Card} from '../../components';
-import GroupButton from '../../components/GroupButton';
-import ListCards from '../../components/ListCards';
-import Navbar from '../../components/Navbar';
+import {Card, Grid} from '../../components';
 import pokemons from '../../data/mock';
-import {ActionsWrapper, Container, ListWrapper} from './list.elements';
-import {EnhancedListProps} from './list.hoc';
 
-const List: React.SFC<EnhancedListProps> = ({
-  renderType,
-  setRenderType,
-  theme,
-  setTheme,
-}) => (
-  <ListWrapper>
-    <Navbar title="TypeScript + Emotion = ♥" />
-    <Container>
-      <ActionsWrapper>
-        <GroupButton>
-          <Button
-            // tslint:disable-next-line
-            onClick={() => setRenderType('grid')}
-            actived={renderType === 'grid'}
-          >
-            Grid
-          </Button>
-          <Button
-            // tslint:disable-next-line
-            onClick={() => setRenderType('list')}
-            actived={renderType === 'list'}
-          >
-            List
-          </Button>
-        </GroupButton>
-        <GroupButton>
-          <Button
-            // tslint:disable-next-line
-            onClick={() => setTheme('primary')}
-            actived={theme === 'primary'}
-          >
-            Primary
-          </Button>
-          <Button
-            // tslint:disable-next-line
-            onClick={() => setTheme('secondary')}
-            actived={theme === 'secondary'}
-          >
-            Secondary
-          </Button>
-        </GroupButton>
-      </ActionsWrapper>
-      <ListCards renderType={renderType}>
-        {pokemons.map((pokemon) => (
-          <Card
-            key={pokemon.name}
-            title={pokemon.name}
-            image={pokemon.avatar}
-          />
-        ))}
-      </ListCards>
-    </Container>
-  </ListWrapper>
+const List: React.SFC<any> = ({renderType}) => (
+  <Grid renderType={renderType}>
+    {pokemons.map((pokemon) => (
+      <Card key={pokemon.name} title={pokemon.name} image={pokemon.avatar} />
+    ))}
+  </Grid>
 );
 
-export default List;
+export {List};
--- src/pages/list/index.ts
+++ src/pages/list/index.ts
@@ -1,4 +1,1 @@
-import List from './List';
-import enhance from './list.hoc';
-
-export default enhance(List);
+export * from './List';
