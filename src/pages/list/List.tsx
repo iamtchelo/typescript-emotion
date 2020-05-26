4665cd21498ed4178044eb55d631b38a315e6723
Marcelo Silva <marcelosilva.developer@gmail.com>
2020-05-25 22:51:35 -0300

    Retrieve pokemons from v2 api version

--- src/pages/list/List.tsx
+++ src/pages/list/List.tsx
@@ -1,13 +1,53 @@
 import * as React from 'react';
 import {Card, Grid} from '../../components';
-import pokemons from '../../data/mock';
+import {pokemonService} from '../../services';
 
-const List: React.SFC<any> = ({renderType}) => (
-  <Grid renderType={renderType}>
-    {pokemons.map((pokemon) => (
-      <Card key={pokemon.name} title={pokemon.name} image={pokemon.avatar} />
-    ))}
-  </Grid>
-);
+const List: React.FunctionComponent<any> = ({renderType}) => {
+  const service = React.useMemo(() => pokemonService(), []);
+  const [response, setResponse] = React.useState({
+    data: [],
+    error: false,
+    loading: false,
+  });
+
+  React.useEffect(() => {
+    const fetchPokemons = async () => {
+      setResponse({...response, loading: true});
+
+      try {
+        const data = await service.fetchPokemons({limit: 300});
+        setResponse({
+          data,
+          error: false,
+          loading: false,
+        });
+      } catch {
+        setResponse({...response, loading: false, error: true});
+      }
+    };
+
+    fetchPokemons();
+  }, []);
+
+  if (response.loading) {
+    return <span>Fetching pokemons...</span>;
+  }
+
+  if (response.error) {
+    return <span>Error while fetching pokemons :/</span>;
+  }
+
+  return (
+    <Grid renderType={renderType}>
+      {response.data.map((pokemon: any) => (
+        <Card
+          key={pokemon.name}
+          title={pokemon.name}
+          image={service.getPokemonURL(pokemon)}
+        />
+      ))}
+    </Grid>
+  );
+};
 
 export {List};
